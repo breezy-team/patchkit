ef12d5dfb186b64db20896a75e8abf7f65f3094c
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-07-01 17:17:56 +0100

    Add iter_lines_handle_nl

--- src/parse.rs
+++ src/parse.rs
@@ -49,3 +49,52 @@ mod splitlines_tests {
         assert_eq!(lines, vec![&b"line 1\n"[..], &b"\n"[..], &b"line 3\n"[..]]);
     }
 }
+
+pub const NO_NL: &[u8] = b"\\ No newline at end of file\n";
+
+/// Iterate through a series of lines, ensuring that lines
+/// that originally had no terminating newline are produced
+/// without one.
+pub fn iter_lines_handle_nl<'a, I>(mut iter_lines: I) -> impl Iterator<Item = &'a [u8]> + 'a
+where
+    I: Iterator<Item = &'a [u8]> + 'a
+{
+    let mut last_line: Option<&'a [u8]> = None;
+    std::iter::from_fn(move || {
+        for line in iter_lines.by_ref() {
+            if line == NO_NL {
+                if let Some(last) = last_line.as_mut() {
+                    assert!(last.ends_with(b"\n"));
+                    // Drop the last newline from `last`
+                    *last = &last[..last.len() - 1];
+                } else {
+                    panic!("No newline indicator without previous line");
+                }
+            } else {
+                if let Some(last) = last_line.take() {
+                    last_line = Some(line);
+                    return Some(last);
+                }
+                last_line = Some(line);
+            }
+        }
+        last_line.take()
+    })
+}
+
+#[test]
+fn test_iter_lines_handle_nl() {
+    let lines = vec![
+        &b"line 1\n"[..],
+        &b"line 2\n"[..],
+        &b"line 3\n"[..],
+        &b"line 4\n"[..],
+        &b"\\ No newline at end of file\n"[..]
+    ];
+    let mut iter = iter_lines_handle_nl(lines.into_iter());
+    assert_eq!(iter.next(), Some("line 1\n".as_bytes()));
+    assert_eq!(iter.next(), Some("line 2\n".as_bytes()));
+    assert_eq!(iter.next(), Some("line 3\n".as_bytes()));
+    assert_eq!(iter.next(), Some("line 4".as_bytes()));
+    assert_eq!(iter.next(), None);
+}
