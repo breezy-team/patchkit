e023e2b54730ff26c51cc4d9a3f5979ec2bc1b6f
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-07-03 23:14:03 +0100

    Add strip_prefix

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,15 @@ pub mod quilt;
 pub mod timestamp;
 pub mod patch;
 pub mod parse;
+
+// TODO: Return a Path instead of a PathBuf
+pub fn strip_prefix(path: &std::path::Path, prefix: usize) -> std::path::PathBuf {
+    path.components().skip(prefix).collect()
+}
+
+#[test]
+fn test_strip_prefix() {
+    assert_eq!(std::path::PathBuf::from("b"), strip_prefix(std::path::Path::new("a/b"), 1));
+    assert_eq!(std::path::PathBuf::from("a/b"), strip_prefix(std::path::Path::new("a/b"), 0));
+    assert_eq!(std::path::PathBuf::from(""), strip_prefix(std::path::Path::new("a/b"), 2));
+}
