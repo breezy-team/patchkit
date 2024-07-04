21ba9c89c8e6fb05c43f7cc8dcdf286fad189b22
Jelmer Vernooij <jelmer@jelmer.uk>
2024-07-04 21:12:27 +0100

    Add example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,43 @@
+//! A crate for parsing and manipulating patches.
+//! 
+//! # Examples
+//! 
+//! ```
+//! use patchkit::parse::parse_patch;
+//! use patchkit::patch::{Patch as _, UnifiedPatch, Hunk, HunkLine};
+//!
+//! let patch = UnifiedPatch::parse_patch(vec![
+//!     "--- a/file1\n",
+//!     "+++ b/file1\n",
+//!     "@@ -1,1 +1,1 @@\n",
+//!     "-a\n",
+//!     "+b\n",
+//! ].into_iter().map(|s| s.as_bytes())).unwrap();
+//!
+//! assert_eq!(patch, UnifiedPatch {
+//!     orig_name: b"a/file1".to_vec(),
+//!     mod_name: b"b/file1".to_vec(),
+//!     orig_ts: None,
+//!     mod_ts: None,
+//!     hunks: vec![
+//!         Hunk {
+//!             mod_pos: 1,
+//!             mod_range: 1,
+//!             orig_pos: 1,
+//!             orig_range: 1,
+//!             lines: vec![
+//!                 HunkLine::RemoveLine(b"a\n".to_vec()),
+//!                 HunkLine::InsertLine(b"b\n".to_vec()),
+//!             ],
+//!             tail: None
+//!         },
+//!     ],
+//! });
+//! 
+//! let applied = patch.apply_exact(&b"a\n"[..]).unwrap();
+//! assert_eq!(applied, b"b\n");
+//! ```
+
 pub mod quilt;
 pub mod timestamp;
 pub mod patch;
